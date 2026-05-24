99a9a58acc52b4efe8e9d6a78122313cc8bfceb1
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-05-24 20:07:23 +0900

    [level 2] Title: [3차] n진수 게임, Time: 7.03 ms, Memory: 64.2 MB -BaekjoonHub

---
+++ Java/프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int n, int t, int m, int p) {
+        StringBuilder sb = new StringBuilder();
+        StringBuilder answer = new StringBuilder();
+
+        int num = 0;
+
+        while (sb.length() < t * m) {
+            sb.append(Integer.toString(num, n).toUpperCase());
+            num++;
+        }
+
+        for (int i = p - 1; answer.length() < t; i += m) {
+            answer.append(sb.charAt(i));
+        }
+
+        return answer.toString();
+    }
+}
