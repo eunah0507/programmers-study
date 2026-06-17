9c492297f11dc34983ba311066912f5870d91d1d
eunah0507 <110543783+eunah0507@users.noreply.github.com>
2026-06-17 22:31:25 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 74.4 MB -BaekjoonHub

---
+++ Java/프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String solution(String myString) {
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : myString.toCharArray()) {
+            if (c == 'a' || c == 'A') {
+                sb.append('A');
+            } else if (c >= 'B' && c <= 'Z') {
+                sb.append((char) (c + 32));
+            } else {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
